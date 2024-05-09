64573b4d30f93f70ae855d0051f5d7cbb815837f
Artem Panfilov <apanfilov@vcstudent.org>
2024-05-09 09:48:53 -0700

    Registered IJSRuntime to services container.

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MessagingClient;
 using MudBlazor.Services;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IJSRuntime, JSRuntime>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
