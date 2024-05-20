f0d29414a6b727f77b80e124cb2587f67be375ee
Artem Panfilov <apanfilov@vcstudent.org>
2024-05-20 08:44:22 -0700

    Created Register Integration Test.

---
+++ tests/Application.IntegrationTests/Services/AuthenticationServiceTests.cs
@@ -0,0 +1,24 @@
+using Application.Interfaces;
+using Application.Services;
+
+namespace Application.IntegrationTests.Services
+{
+    public class AuthenticationServiceTests
+    {
+        private readonly IAuthenticationService _authenticationService;
+        public AuthenticationServiceTests()
+        {
+            var httpClient = new HttpClient{ BaseAddress = new Uri("http://localhost:5000") };
+
+            _authenticationService = new AuthenticationService(httpClient);
+        }
+
+        [Fact]
+        public async void ClientSendsRegisterRequest()
+        {
+            var request = await _authenticationService.Register("Artem", "123");
+
+            Assert.True(request.Message is not null);
+        }
+    }
+}
