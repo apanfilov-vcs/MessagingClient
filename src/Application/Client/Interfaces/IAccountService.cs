4d0b91b5f981a5ae026db8073bc5b0e94bb0f721
Artem Panfilov <apanfilov@vcstudent.org>
2024-05-27 20:53:22 -0700

    Added IAccountService.cs and default implementation.

---
+++ src/Application/Client/Interfaces/IAccountService.cs
@@ -0,0 +1,10 @@
+namespace Application.Client.Interfaces
+{
+    public interface IAccountService
+    {
+        Task LoginAsync(string username, string password);
+        Task LogoutAsync();
+        Task RegisterAsync(string username, string password);
+        Task<bool> IsSessionValidAsync();
+    }
+}
---
+++ src/Application/Client/Services/AccountService.cs
@@ -0,0 +1,62 @@
+using Application.API.Interfaces;
+using Application.Client.Interfaces;
+using Application.DataProcessing;
+
+namespace Application.Client.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAuthenticationService _authenticationService;
+        private readonly ITokenService _tokenService;
+        public AccountService(IAuthenticationService authenticationService, ITokenService tokenService)
+        {
+            _authenticationService = authenticationService;
+            _tokenService = tokenService;
+        }
+
+        public async Task LoginAsync(string username, string password)
+        {
+            var response = await _authenticationService.Login(username, password);
+            
+            if (response.Success is false || response.Data is null)
+            {
+                if (response.Message is not null)
+                {
+                    throw new Exception(response.Message);
+                }
+
+                throw new Exception("Failed to login.");
+            }
+
+            await _tokenService.SaveTokenAsync(response.Data.Value);
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _tokenService.RemoveTokenAsync();
+        }
+
+        public async Task RegisterAsync(string username, string password)
+        {
+            var response = await _authenticationService.Register(username, password);
+
+            if (response.Success is false || response.Data is null)
+            {
+                if (response.Message is not null)
+                {
+                    throw new Exception(response.Message);
+                }
+                
+                throw new Exception("Failed to register.");
+            }
+        }
+
+        public async Task<bool> IsSessionValidAsync()
+        {
+            var token = await _tokenService.GetTokenAsync();
+            var isTokenExpired = TokenHelper.IsTokenExpired(token);
+
+            return isTokenExpired;
+        }
+    }
+}
