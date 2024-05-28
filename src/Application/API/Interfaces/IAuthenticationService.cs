b348186a8ff42782a9fb31fb48c7e8e9de1b70c1
Artem Panfilov <apanfilov@vcstudent.org>
2024-05-28 14:45:37 -0700

    Added nullable options.

--- src/Application/API/Interfaces/IAuthenticationService.cs
+++ src/Application/API/Interfaces/IAuthenticationService.cs
@@ -5,7 +5,7 @@ namespace Application.API.Interfaces
 {
     public interface IAuthenticationService
     {
-        Task<ServiceResponse<GetAuthenticationTokenDto>> Login(string username, string password);
-        Task<ServiceResponse<GetUserDto>> Register(string username, string password);
+        Task<ServiceResponse<GetAuthenticationTokenDto>?> Login(string username, string password);
+        Task<ServiceResponse<GetUserDto>?> Register(string username, string password);
     }
 }
--- src/Application/API/Services/AuthenticationService.cs
+++ src/Application/API/Services/AuthenticationService.cs
@@ -16,7 +16,7 @@ namespace Application.API.Services
             _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
         }
 
-        public async Task<ServiceResponse<GetAuthenticationTokenDto>> Login(string username, string password)
+        public async Task<ServiceResponse<GetAuthenticationTokenDto>?> Login(string username, string password)
         {
             var urlWithParameters = QueryUtility.Authentication.CreateAuthenticationQuery("/api/Authentication/Login?", username, password);
 
@@ -29,26 +29,21 @@ namespace Application.API.Services
 
             var content = await response.Content.ReadFromJsonAsync<ServiceResponse<GetAuthenticationTokenDto>>();
 
-            if (content is null)
-            {
-                throw new NullReferenceException("Unable to load/fetch content.");
-            }
-
             return content;
         }
 
-        public async Task<ServiceResponse<GetUserDto>> Register(string username, string password)
+        public async Task<ServiceResponse<GetUserDto>?> Register(string username, string password)
         {
             var urlWithParameters = QueryUtility.Authentication.CreateAuthenticationQuery("/api/Authentication/Register?", username, password);
-
+            
             var response = await _httpClient.PostAsync(urlWithParameters, null);
+            
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Endpoint request failed.");
+            }
 
             var content = await response.Content.ReadFromJsonAsync<ServiceResponse<GetUserDto>>();
-
-            if (content is null)
-            {
-                throw new NullReferenceException("Unable to load/fetch content");
-            }
 
             return content;
         }
