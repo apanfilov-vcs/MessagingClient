0d6975121724dce7f7341380d80d050e6c90ce8d
Artem Panfilov <apanfilov@vcstudent.org>
2024-05-31 20:48:37 -0700

    Registered services to build container.

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,7 +1,12 @@
+using Application.API.Interfaces;
+using Application.API.Services;
+using Application.Client.Interfaces;
+using Application.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Web;
+using Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +14,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
 builder.Services.AddMudServices();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 
 await builder.Build().RunAsync();
